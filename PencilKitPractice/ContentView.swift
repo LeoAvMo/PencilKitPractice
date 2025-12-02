a96cba0f3c81cac7ecc6b439cd2b35b2b63d5424
LeoAvMo <avilamolinaleonardo@gmail.com>
2025-12-02 18:22:38 +0100

    Initial Commit

---
+++ PencilKitPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PencilKitPractice
+//
+//  Created by Leo A.Molina on 02/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PencilKitPractice/PencilKitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PencilKitPracticeApp.swift
+//  PencilKitPractice
+//
+//  Created by Leo A.Molina on 02/12/25.
+//
+
+import SwiftUI
+
+@main
+struct PencilKitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
